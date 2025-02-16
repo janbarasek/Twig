a544dc8b33d2793859ca7c061956872b64722d6e
Christian Flothmann <christian.flothmann@open.de>
2025-02-16 22:31:07 +0100

    fix the rst syntax of the operator precedence table

--- bin/generate_operators_precedence.php
+++ bin/generate_operators_precedence.php
@@ -21,19 +21,24 @@ require_once \dirname(__DIR__).'/vendor/autoload.php';
 $output = fopen(\dirname(__DIR__).'/doc/operators_precedence.rst', 'w');
 
 $twig = new Environment(new ArrayLoader([]));
+$descriptionLength = 11;
 $expressionParsers = [];
 foreach ($twig->getExpressionParsers() as $expressionParser) {
     $expressionParsers[] = $expressionParser;
+    $descriptionLength = max($descriptionLength, $expressionParser instanceof ExpressionParserDescriptionInterface ? strlen($expressionParser->getDescription()) : '');
 }
 
-fwrite($output, "\n=========== ================ ======= ============= ===========\n");
-fwrite($output, "Precedence  Operator         Type    Associativity Description\n");
-fwrite($output, '=========== ================ ======= ============= ===========');
+fwrite($output, "\n+------------+------------------+---------+---------------+".str_repeat('-', $descriptionLength + 2)."+\n");
+fwrite($output, "| Precedence | Operator         | Type    | Associativity | Description".str_repeat(' ', $descriptionLength - 11)." |\n");
+fwrite($output, '+============+==================+=========+===============+'.str_repeat('=', $descriptionLength + 2).'+');
 
 usort($expressionParsers, fn ($a, $b) => $b->getPrecedence() <=> $a->getPrecedence());
 
 $previous = null;
 foreach ($expressionParsers as $expressionParser) {
+    if (null !== $previous) {
+        fwrite($output, "\n+------------+------------------+---------+---------------+".str_repeat('-', $descriptionLength + 2).'+');
+    }
     $precedence = $expressionParser->getPrecedence();
     $previousPrecedence = $previous ? $previous->getPrecedence() : \PHP_INT_MAX;
     $associativity = $expressionParser instanceof InfixExpressionParserInterface ? (InfixAssociativity::Left === $expressionParser->getAssociativity() ? 'Left' : 'Right') : 'n/a';
@@ -41,7 +46,7 @@ foreach ($expressionParsers as $expressionParser) {
     if ($previousPrecedence !== $precedence) {
         $previous = null;
     }
-    fwrite($output, rtrim(\sprintf("\n%-11s %-16s %-7s %-13s %s\n",
+    fwrite($output, rtrim(\sprintf("\n| %-10s | %-16s | %-7s | %-13s | %-{$descriptionLength}s |\n",
         (!$previous || $previousPrecedence !== $precedence ? $precedence : '').($expressionParser->getPrecedenceChange() ? ' => '.$expressionParser->getPrecedenceChange()->getNewPrecedence() : ''),
         '``'.$expressionParser->getName().'``',
         !$previous || ExpressionParserType::getType($previous) !== ExpressionParserType::getType($expressionParser) ? ExpressionParserType::getType($expressionParser)->value : '',
@@ -50,14 +55,14 @@ foreach ($expressionParsers as $expressionParser) {
     )));
     $previous = $expressionParser;
 }
-fwrite($output, "\n=========== ================ ======= ============= ===========\n");
+fwrite($output, "\n+------------+------------------+---------+---------------+".str_repeat('-', $descriptionLength + 2)."+\n");
 fwrite($output, "\nWhen a precedence will change in 4.0, the new precedence is indicated by the arrow ``=>``.\n");
 
 fwrite($output, "\nHere is the same table for Twig 4.0 with adjusted precedences:\n");
 
-fwrite($output, "\n=========== ================ ======= ============= ===========\n");
-fwrite($output, "Precedence  Operator         Type    Associativity Description\n");
-fwrite($output, '=========== ================ ======= ============= ===========');
+fwrite($output, "\n+------------+------------------+---------+---------------+".str_repeat('-', $descriptionLength + 2)."+\n");
+fwrite($output, "| Precedence | Operator         | Type    | Associativity | Description".str_repeat(' ', $descriptionLength - 11)." |\n");
+fwrite($output, '+============+==================+=========+===============+'.str_repeat('=', $descriptionLength + 2).'+');
 
 usort($expressionParsers, function ($a, $b) {
     $aPrecedence = $a->getPrecedenceChange() ? $a->getPrecedenceChange()->getNewPrecedence() : $a->getPrecedence();
@@ -68,6 +73,9 @@ usort($expressionParsers, function ($a, $b) {
 
 $previous = null;
 foreach ($expressionParsers as $expressionParser) {
+    if (null !== $previous) {
+        fwrite($output, "\n+------------+------------------+---------+---------------+".str_repeat('-', $descriptionLength + 2).'+');
+    }
     $precedence = $expressionParser->getPrecedenceChange() ? $expressionParser->getPrecedenceChange()->getNewPrecedence() : $expressionParser->getPrecedence();
     $previousPrecedence = $previous ? ($previous->getPrecedenceChange() ? $previous->getPrecedenceChange()->getNewPrecedence() : $previous->getPrecedence()) : \PHP_INT_MAX;
     $associativity = $expressionParser instanceof InfixExpressionParserInterface ? (InfixAssociativity::Left === $expressionParser->getAssociativity() ? 'Left' : 'Right') : 'n/a';
@@ -75,7 +83,7 @@ foreach ($expressionParsers as $expressionParser) {
     if ($previousPrecedence !== $precedence) {
         $previous = null;
     }
-    fwrite($output, rtrim(\sprintf("\n%-11s %-16s %-7s %-13s %s\n",
+    fwrite($output, rtrim(\sprintf("\n| %-10s | %-16s | %-7s | %-13s | %-{$descriptionLength}s |\n",
         !$previous || $previousPrecedence !== $precedence ? $precedence : '',
         '``'.$expressionParser->getName().'``',
         !$previous || ExpressionParserType::getType($previous) !== ExpressionParserType::getType($expressionParser) ? ExpressionParserType::getType($expressionParser)->value : '',
@@ -84,6 +92,6 @@ foreach ($expressionParsers as $expressionParser) {
     )));
     $previous = $expressionParser;
 }
-fwrite($output, "\n=========== ================ ======= ============= ===========\n");
+fwrite($output, "\n+------------+------------------+---------+---------------+".str_repeat('-', $descriptionLength + 2)."+\n");
 
 fclose($output);
